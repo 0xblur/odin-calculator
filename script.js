feb907d0c8984c7f9063279191eeb40c25869b42
cypherHades <xjadezbix@gmx.com>
2023-12-26 11:05:21 -0500

    Operator functions.
    Expects two operands (a and b) in the format of a string.

---
+++ script.js
@@ -0,0 +1,16 @@
+// Functions
+function add(a, b) {
+  return Number(a) + Number(b)
+}
+
+function subtract(a, b) {
+  return Number(a) - Number(b)
+}
+
+function multiply(a, b) {
+  return Number(a) * Number(b)
+}
+
+function divide(a, b) {
+  return Number(a) / Number(b)
+}
