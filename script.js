3cae2569090de7ce22233746198a20e29c455298
cypherHades <xjadezbix@gmx.com>
2023-12-26 11:06:24 -0500

    Node Elements initialized.
    Possibly not all will be needed.

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Elements
+const a = document.querySelector("#a");
+const b = document.querySelector("#b");
+const operator = document.querySelector("#operator");
+const display = document.querySelector("#display");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operators button");
+const clearBtn = document.querySelector("#clear");
+const deleteBtn = document.querySelector("#delete");
+const calculateBtn = document.querySelector("#equals");
+
 // Functions
 function add(a, b) {
   return Number(a) + Number(b)
